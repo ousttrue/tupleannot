006c24194deef6ff58a1d30956d0553bdc1d7ddf
ousttrue <ousttrue@gmail.com>
2019-06-05 10:47:52 +0900

    WIP use metaclass

--- python/tests/test_sample.py
+++ python/tests/test_sample.py
@@ -2,29 +2,29 @@ import unittest
 from tupleannot import *
 
 
+# type definition
+class Vec3(Tuple):
+    x: Float
+    y: Float
+    z: Float
+
+
 class SampleTest(unittest.TestCase):
     def test_sample(self):
         # binary data
         import struct
         data = struct.pack('ffffffd', 1, 2, 3, 4, 5, 6, 7)
 
-        # type definition
-        vec3 = Tuple('Vec3', [
-            Float('x'),
-            Float('y'),
-            Float('z'),
-        ])
-
         # parse / get value
-        remain = vec3.parse(data) # consume bytes
-        self.assertEquals(1, vec3['x'])
-        self.assertEquals(2, vec3['y'])
-        self.assertEquals(3, vec3['z'])
+        vec3, remain = Vec3[2].parse(data) # consume bytes
+        self.assertEquals(1, vec3[0]['x'])
+        self.assertEquals(2, vec3[0]['y'])
+        self.assertEquals(3, vec3[0]['z'])
+        self.assertEquals(4, vec3[1]['x'])
+        self.assertEquals(5, vec3[1]['y'])
+        self.assertEquals(6, vec3[1]['z'])
 
-        remain = vec3.parse(remain) # consume bytes
-
-        d = Double('d')
-        remain = d.parse(remain)
+        d, remain = Double.parse(remain)
         self.assertEquals(bytes(), remain)
         self.assertEquals(7, d.value())
 
