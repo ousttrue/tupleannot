348fd95d6aac24ca03b1e908c3674044541e8206
ousttrue <ousttrue@gmail.com>
2019-06-06 02:57:35 +0900

    separate primitive.py

--- python/tupleannot/__init__.py
+++ python/tupleannot/__init__.py
@@ -1,120 +1,15 @@
 import struct
 import collections
 import typing
+try:
+    from .primitive import *
+except ImportError as ex:
+    from primitive import *
 
 VERSION = [0, 1, 0]
 
 
-class _MetaDefinition(type):
-    @classmethod
-    def __prepare__(metacls, name, bases, **kwds):
-        return collections.OrderedDict()
-
-    def __getitem__(self, length: int):
-        '''
-        create Array class by []. ex: UInt32[4]
-
-        ToDo: negative length
-        '''
-
-        class Array(self):
-            __element_size__ = self.__element_size__
-            __length__ = length
-
-            def __init__(self, segment: bytes) -> None:
-                super().__init__(segment)
-                self.reference = None
-
-            def __getitem__(self, i: int) -> self:
-                size = self.__class__.__element_size__
-                cls = self.__class__.__bases__[0]
-                begin = size * i
-                end = begin + size
-                return cls(self.segment[begin:end])
-
-            def value(self):
-                size = self.__class__.__element_size__
-                return [
-                    self.__class__.__bases__[0](self.segment[x:x + size]).value()
-                    for x in range(0, len(self.segment), size)
-                ]
-
-            @classmethod
-            def parse(cls, src: bytes) -> typing.Tuple[bytes, bytes]:
-                s = cls.__element_size__ * cls.__length__
-                value = src[0:s]
-                remain = src[s:]
-                return cls(value), remain
-
-        return Array
-
-
-class Base(metaclass=_MetaDefinition):
-    def __init__(self, segment: bytes) -> None:
-        self.segment = segment
-
-    def value(self):
-        return struct.unpack(f'{self.__class__.__fmt__}', self.segment)[0]
-
-    @classmethod
-    def parse(cls, src: bytes) -> typing.Tuple[bytes, bytes]:
-        s = cls.__element_size__
-        value = src[0:s]
-        remain = src[s:]
-        return cls(value), remain
-
-
-class Int8(Base):
-    __fmt__ = 'b'
-    __element_size__ = 1
-
-
-class Int16(Base):
-    __fmt__ = 'h'
-    __element_size__ = 2
-
-
-class Int32(Base):
-    __fmt__ = 'i'
-    __element_size__ = 4
-
-
-class Int64(Base):
-    __fmt__ = 'q'
-    __element_size__ = 8
-
-
-class UInt8(Base):
-    __fmt__ = 'B'
-    __element_size__ = 1
-
-
-class UInt16(Base):
-    __fmt__ = 'H'
-    __element_size__ = 2
-
-
-class UInt32(Base):
-    __fmt__ = 'I'
-    __element_size__ = 4
-
-
-class UInt64(Base):
-    __fmt__ = 'Q'
-    __element_size__ = 8
-
-
-class Float(Base):
-    __fmt__ = 'f'
-    __element_size__ = 4
-
-
-class Double(Base):
-    __fmt__ = 'd'
-    __element_size__ = 8
-
-
-class _TupleMeta(_MetaDefinition):
+class _TupleMeta(MetaDefinition):
     def __new__(metacls, name, bases, namespace, **kwds):
         annotations = namespace.get('__annotations__')
         if annotations:
@@ -124,12 +19,13 @@ class _TupleMeta(_MetaDefinition):
                 element_size += v.__element_size__
 
             # Create Tuple
-            class _Tuple(metaclass=_MetaDefinition):
+            class _Tuple(metaclass=MetaDefinition):
                 __element_size__ = element_size
                 __tuple_items__ = annotations
 
-                def __init__(self, segment: bytes) -> None:
+                def __init__(self, segment: bytes, parent) -> None:
                     self.segment = segment
+                    self.parent = parent
                     self.values = []
 
                 def __getitem__(self, key: str):
@@ -164,7 +60,8 @@ class _TupleMeta(_MetaDefinition):
                     }
 
                 @classmethod
-                def parse(cls, data: bytes) -> typing.Tuple[bytes, bytes]:
+                def parse(cls, data: bytes,
+                          parent=None) -> typing.Tuple[bytes, bytes]:
                     it = iter(annotations.items())
                     src = data
                     size = 0
@@ -175,14 +72,15 @@ class _TupleMeta(_MetaDefinition):
                             size += len(src)
                         except StopIteration:
                             break
-                    return cls(data[0:size]), src
+                    return cls(data[0:size], parent), src
 
             return _Tuple
         else:
             return type.__new__(metacls, name, bases, dict(namespace))
 
 
-class TypedTuple(metaclass=_TupleMeta): pass
+class TypedTuple(metaclass=_TupleMeta):
+    pass
 
 
 def main():
---
+++ python/tupleannot/primitive.py
@@ -0,0 +1,140 @@
+import collections
+import struct
+from typing import NamedTuple, Any, Tuple
+
+
+class ParentWithIndex(NamedTuple):
+    parent: Any
+    index: int
+
+
+class MetaDefinition(type):
+    @classmethod
+    def __prepare__(metacls, name, bases, **kwds):
+        return collections.OrderedDict()
+
+    def __getitem__(self, length: int):
+        '''
+        create Array class by []. ex: UInt32[4]
+
+        ToDo: negative length(only Tuple member)
+        '''
+
+        def parse_fixed_array(cls, src: bytes, parent: ParentWithIndex) -> Tuple[Base, bytes]:
+            s = cls.__element_size__ * cls.__length__
+            value = src[0:s]
+            remain = src[s:]
+            return cls(value, parent), remain
+
+        class Array(self):
+            __element_size__ = self.__element_size__
+            __length__ = length
+
+            def __init__(self, segment: bytes,
+                         parent: ParentWithIndex) -> None:
+                super().__init__(segment, parent)
+                self.reference = None
+
+            def __getitem__(self, i: int) -> self:
+                size = self.__class__.__element_size__
+                cls = self.__class__.__bases__[0]
+                begin = size * i
+                end = begin + size
+                return cls(self.segment[begin:end], ParentWithIndex(self, i))
+
+            def value(self):
+                return [self[i] for i in range(0, self.__class__.__length__)]
+
+            @classmethod
+            def parse(cls, src: bytes, parent=None) -> Tuple[Base, bytes]:
+                return parse_fixed_array(cls, src, parent)
+
+        return Array
+
+
+class Base(metaclass=MetaDefinition):
+    def __init__(self, segment: bytes, parent: ParentWithIndex) -> None:
+        self.segment = segment
+        self.parent = parent
+
+
+class Primitive(Base):
+    '''
+    Int8, 16, 32, 64
+    UInt8, 16, 32, 64
+    Float, Double
+    '''
+
+    def value(self):
+        return struct.unpack(f'{self.__class__.__fmt__}', self.segment)[0]
+
+    @classmethod
+    def parse(cls, src: bytes, parent=None) -> Tuple[bytes, bytes]:
+        s = cls.__element_size__
+        value = src[0:s]
+        remain = src[s:]
+        return cls(value, parent), remain
+
+
+class Int8(Primitive):
+    __fmt__ = 'b'
+    __element_size__ = 1
+
+
+class Int16(Primitive):
+    __fmt__ = 'h'
+    __element_size__ = 2
+
+
+class Int32(Primitive):
+    __fmt__ = 'i'
+    __element_size__ = 4
+
+
+class Int64(Primitive):
+    __fmt__ = 'q'
+    __element_size__ = 8
+
+
+class UInt8(Primitive):
+    __fmt__ = 'B'
+    __element_size__ = 1
+
+
+class UInt16(Primitive):
+    __fmt__ = 'H'
+    __element_size__ = 2
+
+
+class UInt32(Primitive):
+    __fmt__ = 'I'
+    __element_size__ = 4
+
+
+class UInt64(Primitive):
+    __fmt__ = 'Q'
+    __element_size__ = 8
+
+
+class Float(Primitive):
+    __fmt__ = 'f'
+    __element_size__ = 4
+
+
+class Double(Primitive):
+    __fmt__ = 'd'
+    __element_size__ = 8
+
+
+def main():
+    data = struct.pack('6I', 1, 2, 3, 4, 5, 6)
+    parsed, remain = UInt32.parse(data)
+    print(f'UInt32: {parsed.value()}')
+
+    parsed, remain = UInt32[6].parse(data)
+    print(f'UInt32[0]: {parsed[0].value()}')
+    print(f'UInt32[5]: {parsed[5].value()}')
+
+
+if __name__ == '__main__':
+    main()
